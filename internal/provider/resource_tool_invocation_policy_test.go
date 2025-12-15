4d6467efba66f39a29a686d23024a4419d614ecd
Excellencedev <ademiluyisuccessandexcellence@gmail.com>
2025-12-15 17:55:40 +0100

    Fix builds

--- internal/provider/resource_tool_invocation_policy_test.go
+++ internal/provider/resource_tool_invocation_policy_test.go
@@ -13,7 +13,6 @@ import (
 
 func TestAccToolInvocationPolicyResource(t *testing.T) {
 	rName := acctest.RandStringFromCharSet(8, acctest.CharSetAlphaNum)
-
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
@@ -58,6 +57,11 @@ func TestAccToolInvocationPolicyResource(t *testing.T) {
 				ConfigStateChecks: []statecheck.StateCheck{
 					statecheck.ExpectKnownValue(
 						"archestra_tool_invocation_policy.test",
+						tfjsonpath.New("argument_name"),
+						knownvalue.StringExact("path"),
+					),
+					statecheck.ExpectKnownValue(
+						"archestra_tool_invocation_policy.test",
 						tfjsonpath.New("operator"),
 						knownvalue.StringExact("startsWith"),
 					),
@@ -65,6 +69,16 @@ func TestAccToolInvocationPolicyResource(t *testing.T) {
 						"archestra_tool_invocation_policy.test",
 						tfjsonpath.New("value"),
 						knownvalue.StringExact("/var/log/"),
+					),
+					statecheck.ExpectKnownValue(
+						"archestra_tool_invocation_policy.test",
+						tfjsonpath.New("action"),
+						knownvalue.StringExact("allow_when_context_is_untrusted"),
+					),
+					statecheck.ExpectKnownValue(
+						"archestra_tool_invocation_policy.test",
+						tfjsonpath.New("reason"),
+						knownvalue.StringExact("Allow log file access in untrusted contexts"),
 					),
 				},
 			},
@@ -74,7 +88,6 @@ func TestAccToolInvocationPolicyResource(t *testing.T) {
 
 func TestAccToolInvocationPolicyResource_WithoutReason(t *testing.T) {
 	rName := acctest.RandStringFromCharSet(8, acctest.CharSetAlphaNum)
-
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
@@ -98,9 +111,29 @@ func TestAccToolInvocationPolicyResource_WithoutReason(t *testing.T) {
 	})
 }
 
+func testAccToolInvocationPolicyResourceConfigNoReason(rName string) string {
+	return fmt.Sprintf(`
+resource "archestra_profile" "noreason" {
+  name = "tip-noreason-profile-%[1]s"
+}
+
+data "archestra_profile_tool" "noreason" {
+  profile_id = archestra_profile.noreason.id
+  tool_name  = "archestra__whoami"
+}
+
+resource "archestra_tool_invocation_policy" "noreason" {
+  profile_tool_id = data.archestra_profile_tool.noreason.id
+  argument_name   = "command"
+  operator        = "equal"
+  value           = "rm -rf"
+  action          = "block_always"
+}
+`, rName)
+}
+
 func TestAccToolInvocationPolicyResource_RegexOperator(t *testing.T) {
 	rName := acctest.RandStringFromCharSet(8, acctest.CharSetAlphaNum)
-
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
@@ -113,8 +146,79 @@ func TestAccToolInvocationPolicyResource_RegexOperator(t *testing.T) {
 						tfjsonpath.New("operator"),
 						knownvalue.StringExact("regex"),
 					),
+					statecheck.ExpectKnownValue(
+						"archestra_tool_invocation_policy.regex",
+						tfjsonpath.New("value"),
+						knownvalue.StringExact("^/home/[a-z]+/.ssh/.*"),
+					),
 				},
 			},
 		},
 	})
+}
+
+func testAccToolInvocationPolicyResourceConfig(rName string) string {
+	return fmt.Sprintf(`
+resource "archestra_profile" "test" {
+  name = "tip-test-profile-%[1]s"
+}
+
+data "archestra_profile_tool" "test" {
+  profile_id = archestra_profile.test.id
+  tool_name  = "archestra__whoami"
+}
+
+resource "archestra_tool_invocation_policy" "test" {
+  profile_tool_id = data.archestra_profile_tool.test.id
+  argument_name   = "path"
+  operator        = "contains"
+  value           = "/etc/"
+  action          = "block_always"
+  reason          = "Block access to system configuration files"
+}
+`, rName)
+}
+
+func testAccToolInvocationPolicyResourceConfigUpdated(rName string) string {
+	return fmt.Sprintf(`
+resource "archestra_profile" "test" {
+  name = "tip-test-profile-%[1]s"
+}
+
+data "archestra_profile_tool" "test" {
+  profile_id = archestra_profile.test.id
+  tool_name  = "archestra__whoami"
+}
+
+resource "archestra_tool_invocation_policy" "test" {
+  profile_tool_id = data.archestra_profile_tool.test.id
+  argument_name   = "path"
+  operator        = "startsWith"
+  value           = "/var/log/"
+  action          = "allow_when_context_is_untrusted"
+  reason          = "Allow log file access in untrusted contexts"
+}
+`, rName)
+}
+
+func testAccToolInvocationPolicyResourceConfigRegex(rName string) string {
+	return fmt.Sprintf(`
+resource "archestra_profile" "regex" {
+  name = "tip-regex-profile-%[1]s"
+}
+
+data "archestra_profile_tool" "regex" {
+  profile_id = archestra_profile.regex.id
+  tool_name  = "archestra__whoami"
+}
+
+resource "archestra_tool_invocation_policy" "regex" {
+  profile_tool_id = data.archestra_profile_tool.regex.id
+  argument_name   = "path"
+  operator        = "regex"
+  value           = "^/home/[a-z]+/.ssh/.*"
+  action          = "block_always"
+  reason          = "Block SSH key access using regex pattern"
+}
+`, rName)
 }
