aa21a0c54a18303c8457dc82f4738321912ed3e5
Joey Orlando <joey@archestra.ai>
2026-08-13 09:53:21 -0400

    feat: archestra_agent_delegation resource (#154)

---
+++ examples/resources/archestra_agent_delegation/import.sh
@@ -0,0 +1,2 @@
+# Composite ID: <agent_id>:<target_agent_id> (the format Create assigns to id).
+terraform import archestra_agent_delegation.example 00000000-0000-0000-0000-000000000000:11111111-1111-1111-1111-111111111111
